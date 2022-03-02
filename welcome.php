e04d1c8023721880049278e8f106a6970bcb1c8a
Ronodeep Mondal <rono4321@gmail.com>
2022-03-02 19:09:00 +0530

    Updated php form

--- welcome.php
+++ welcome.php
@@ -1,8 +1,13 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+ASSET_ID is <?php echo $_POST["asset_id"]; ?><br>
+VERSION is <?php echo $_POST["version"]; ?><br>
+SERVICE_NAME is <?php echo $_POST["service_name"]; ?><br>
+ENVIRONMENT is <?php echo $_POST["environment"]; ?><br>
+DOCKER_EXTRA is <?php echo $_POST["docker_extra"]; ?><br>
+DOCKER_IMAGE_VERSION is <?php echo $_POST["docker_image_version"]; ?><br>
+
 
 </body>
 </html>
